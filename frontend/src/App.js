4d65129eb0545a99a662ad85df245f5949fc22b6
NSP2023 <noshinsyarapromitee848@gmail.com>
2025-12-16 12:15:48 +0600

    Add StackShare folder to Frontend_initial branch

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BorrowerDashboard from "./BorrowerDashboard";
+import "./App.css";
+import "./BorrowerDashboard.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BorrowerDashboard />
+    </div>
+  );
+}
+
+export default App;
